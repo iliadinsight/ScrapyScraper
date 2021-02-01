ec497844d0ad1faa0567f3b3092db159875d3ed5
Cemlyn <cemlyn.coro@gmail.com>
2021-02-01 14:12:01 +0000

    meh

--- demoscraper/demoscraper/spiders/Posts.py
+++ demoscraper/demoscraper/spiders/Posts.py
@@ -8,15 +8,16 @@ class PostsSpider(scrapy.Spider):
     start_urls = ["https://blog.scrapinghub.com"]
 
     def parse(self,response):
-        for listing in  response.css('div.post-listing'):
-            url = listing.css('a.hs-featured-image-link::attr(href)').get()
-            yield scrapy.Request(url,callback=self.parse_post_contents)
 
-    def parse_post_contents(self,response):
+        post_links = response.css('div.post-listing').css('a.hs-featured-image-link::attr(href)').getall()
+        yield from response.follow_all(post_links, self.parse_post)
+
+    def parse_post(self,response):
         yield {
+            'url':response.url,
             'title':response.css('span.hs_cos_wrapper::text').get(),
             'date': response.css('span.date').css('a::text').get(),
             'author': response.css('span.author').css('a::text').get(),
             'no_comment': response.css('span.custom_listing_comments').css('a::text').get(),
-            #'content': response.css('div.section.post-body').css('p').getall()
+            'content': response.css('span.hs_cos_wrapper.hs_cos_wrapper_meta_field.hs_cos_wrapper_type_rich_text').css('span::text').getall()
         }
